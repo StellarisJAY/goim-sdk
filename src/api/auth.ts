54e7dc499b9bc68ca933d2664cd9514e40eba480
StellarisJAY <760409558@qq.com>
2022-08-15 16:52:44 +0800

    feat: 登录和注册的http请求

---
+++ src/api/auth.ts
@@ -0,0 +1,37 @@
+import request from '../util/request'
+import { BaseResponse, LoginResponse, RegisterRequest } from './base'
+
+export function login(account: string, password: string): Promise<LoginResponse> {
+    let data = {
+        "account": account,
+        "password": password
+    }
+    return new Promise<LoginResponse>((resolve, reject)=>{
+        request.post('/auth/login', data)
+        .then(function(response){
+            resolve({
+                code: response.data.code,
+                message: response.data.message,
+                token: response.data.token
+            } as LoginResponse)
+        })
+        .catch(function(err){
+            reject(err)
+        })
+    })
+}
+
+export function register(regReq: RegisterRequest): Promise<BaseResponse> {
+    return new Promise<BaseResponse>((resolve, reject)=>{
+        request.post('/auth/register', regReq)
+        .then(function(response){
+            resolve({
+                code: response.data.code,
+                message: response.data.message
+            } as BaseResponse)
+        })
+        .catch(function(err){
+
+        })
+    })
+}
