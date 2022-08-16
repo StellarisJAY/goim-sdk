466aa7dcac74cea7c2a995df13e75f14797588c7
StellarisJAY <760409558@qq.com>
2022-08-16 16:18:03 +0800

    feat: 定义用户、消息请求返回

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,3 +1,5 @@
+import { MessageFlag } from "../proto/msg"
+
 export interface BaseResponse {
     code: number
     message: string
@@ -13,4 +15,44 @@ export interface RegisterRequest {
     account: string
     nickName: string
     password: string
+}
+
+export interface SendMessageRequest {
+    to: number
+    content: string
+    flag: number
+}
+
+export interface SendMessageResponse {
+    code: number
+    message: string
+    messageId: number
+    timestamp: number
+}
+
+export interface UserInfo {
+    account: string
+    nickName: string
+    userID: number
+    registerTime:number
+}
+
+export interface UserInfoResponse {
+    code: number
+    message: string
+    userInfo: UserInfo
+}
+
+export interface FriendInfo {
+    account: string
+    nickName: string
+    userID: number
+    registerTime:number
+    acceptTime: number
+}
+
+export interface FriendListResponse {
+    code: number
+    message: string
+    friends: FriendInfo[]
 }
